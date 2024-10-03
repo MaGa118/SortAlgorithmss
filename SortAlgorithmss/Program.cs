01d1a4f5b39691b94573b2f0565304bb8ca7bce6
bi76vy <bi76vy@FOT-CELLA13>
2024-10-03 10:15:36 +0100

    Add project files.

---
+++ SortAlgorithmss/Program.cs
@@ -0,0 +1,71 @@
+﻿using SortAlgorithmss;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class Program
+    {
+        static Random randomGenerator = new Random();
+        static List<double> listOfDoubles = new List<double>();
+
+        static void Main(string[] args)
+        {
+            TenDoubles();
+
+            MenuSystem mainMenu = new MenuSystem("Program");
+
+            mainMenu.AddMenuItem("Randomise List of Doubles (10 Values)", "TenDoubles", typeof(Program));
+            mainMenu.AddMenuItem("Print List of Doubles", "PrintListOfDoubles", typeof(Program));
+            mainMenu.AddMenuItem("Perform a Bubble Sort", "PerformBubble", typeof(Program));
+
+            mainMenu.RunForever();
+
+            Console.ReadKey();
+        }
+
+        public static void PerformBubble()
+        {
+            Console.WriteLine("Performing Bubble Sort...");
+
+            BubbleSort.Perform(listOfDoubles);
+
+            Console.WriteLine("Sort Completed");
+        }
+
+        public static void TenDoubles()
+        {
+            Console.WriteLine("Populating list with random numbers...");
+
+            PopulateListWithRandomDoubles(10);
+
+            Console.WriteLine("List populated");
+        }
+
+        static void PopulateListWithRandomDoubles(int size)
+        {
+            listOfDoubles.Clear();
+
+            for (int i = 0; i < size; i++)
+            {
+                double twoDigitDouble = Double.Parse(randomGenerator.NextDouble().ToString("0.00000"));
+                listOfDoubles.Add(twoDigitDouble);
+            }
+        }
+
+        public static void PrintListOfDoubles()
+        {
+            Console.WriteLine("\n\nLIST PRINT:\n");
+
+            for (int i = 0; i < listOfDoubles.Count; i++)
+            {
+                Console.WriteLine(i + " " + listOfDoubles[i].ToString());
+            }
+
+            Console.WriteLine("\nEND \n");
+        }
+    }
+}
